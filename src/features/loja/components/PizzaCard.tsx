4f9c7b1c6d40ce1bba4ac9bec7bd396cb19ac2a1
bernardo-gontijo <bernardogontijofernandes@gmail.com>
2026-08-29 19:25:25 -0400

    Retirada do botão Detalhes, trocando para o card ser o Link (Mudança no PizzaCard + index + todasAsPizzas)

--- src/features/loja/components/PizzaCard.tsx
+++ src/features/loja/components/PizzaCard.tsx
@@ -13,17 +13,20 @@ export function PizzaCard({ pizza }: PizzaCardProps) {
   }
 
   return (
-    <article className="pizza-card">
-      <img alt={pizza.nome} src={pizza.imagem} onError={usarImagemReserva} />
-      <div className="pizza-card__conteudo">
-        <span className="categoria">{pizza.categoria}</span>
-        <h2>{pizza.nome}</h2>
-        <p>{pizza.descricao}</p>
-        <strong>R$ {pizza.preco.toFixed(2).replace(".", ",")}</strong>
-        <Link className="pizza-card__link" to={`/pizza/${pizza.id}`}>
-          Ver detalhes
-        </Link>
-      </div>
-    </article>
+    <Link
+      className="pizza-card__link"
+      to={`/pizza/${pizza.id}`}
+      arial-label={`Ver detalhes da pizza ${pizza.nome}`}
+    >
+      <article className="pizza-card">
+        <img alt={pizza.nome} src={pizza.imagem} onError={usarImagemReserva} />
+        <div className="pizza-card__conteudo">
+          <span className="categoria">{pizza.categoria}</span>
+          <h2>{pizza.nome}</h2>
+          <p>{pizza.descricao}</p>
+          <strong>R$ {pizza.preco.toFixed(2).replace(".", ",")}</strong>
+        </div>
+      </article>
+    </Link>
   );
 }
