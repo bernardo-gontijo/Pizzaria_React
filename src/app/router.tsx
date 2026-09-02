67910c1af871fe5524badf8a8a0819ba36bfc14d
Kauan Matheus <kauanmmmartins@gmail.com>
2026-09-02 14:27:45 -0400

    refactor: atualiza rota do cozinheiro

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -121,7 +121,7 @@ export const router = createBrowserRouter([
     ],
   },
   {
-    path: "/cozinheiro-teste",
+    path: "/cozinha",
     element: (
       <Suspense fallback={<Loading />}>
         <CozinheiroPage />
